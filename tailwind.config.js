d367c82ec0e12b8136bd6e2246c16a872fc60f32
fatemeh <fatemeh211100@gmail.com>
2024-06-05 14:33:33 +0330

    commit file

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./public/**/*.{html,js}"],
+  theme: {
+    extend: {
+      fontFamily: { sans: ["vazir"] },
+    },
+  },
+  plugins: [
+    require("@tailwindcss/forms")({
+      strategy: "base", // only generate global styles
+      strategy: "class", // only generate classes
+    }),
+  ],
+};
